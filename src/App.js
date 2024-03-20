ebafb70685dd4ec657df7e6244ee12650631aaf1
Brian Kepha <kephareviewsit@gmail.com>
2024-03-20 13:45:28 +0300

    Updated the src/App.js file

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DEPLOYMENT OF A REACT APP USING GITHUB ACTIONS TO AWS</h1>
+        <p>
+         
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/briankepha/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SSUP PEEPS, KEPHA HERE 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
